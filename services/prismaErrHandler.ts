fe97f772ae03f887682b683a26f310c6577c1e2d
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-17 18:39:06 -0800

    Update prismaErrHandler.ts

--- services/prismaErrHandler.ts
+++ services/prismaErrHandler.ts
@@ -13,18 +13,25 @@ export function prismaErrHandler(e: any): string {
           e.message
       );
     }
-    if (e.code == "P2002") {
+    if (e.code == "P2000") {
+      //varChar constraint
+      return "Field " + e.meta?.column_name + " is too long.";
+    } else if (e.code == "P2002") {
+      //unique constraint
       //e.meta.target
       return (
         "Another object with the same " + e.meta?.target + " exists already."
       );
     } else if (e.code == "P2025") {
+      //object DNE
       //e.meta.modelName
       return "Your data is outdated, please refresh your webpage.";
     } else if (e.code == "P2003") {
+      //relation DNE
       //e.meta.modelName, e.meta.field_name
       return "Your data is outdated, please refresh your webpage.";
     } else {
+      //unhandled prisma error
       return "Your data is outdated, please refresh your webpage.";
       //   return "Unhandled error: " + e.code + " | " + e.message;
     }
