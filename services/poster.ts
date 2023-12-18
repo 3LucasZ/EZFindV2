d40db44c8b327e604f625e94f468182151543068
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-17 16:07:42 -0800

    Poster function with bare level fetching

---
+++ services/poster.ts
@@ -0,0 +1,21 @@
+import Router from "next/router";
+import { errorToast, successToast } from "./toasty";
+
+export async function poster(path: string, body: any, toaster: any) {
+  try {
+    const res = await fetch(path, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    if (res.status != 200) {
+      errorToast(toaster, "" + (await res.json()));
+    } else {
+      successToast(toaster, "Success!");
+      return true;
+    }
+  } catch (error) {
+    errorToast(toaster, "Unknown error: " + error);
+  }
+  return false;
+}
