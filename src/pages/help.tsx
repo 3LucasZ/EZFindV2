3b2bd528b6b8aa5be39bcf18765ecacd238d620a
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-19 02:50:27 -0800

    Update help.tsx

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -26,6 +26,35 @@ export default function Home({ admins }: PageProps) {
       <Header isAdmin={isAdmin} />
 
       <Box px="5" overflowY="auto">
+        <Text fontSize="4xl">Version</Text>
+        <Text fontSize="xl">2.2 (Alpha)</Text>
+        <Text fontSize="4xl">Q&A</Text>
+        <Text fontSize="xl">
+          Q: Why is the website considered unsafe and untrusted by Safari and
+          Chrome?
+        </Text>
+        <Text fontSize="xl">
+          A: The website is in the alpha version and running on a local
+          development server. Rest assured, your data is safe. Check out our
+          privacy policy and terms of service below.
+        </Text>
+        <Text fontSize="xl">Q: What does the invert checkbox do?</Text>
+        <Text fontSize="xl">
+          A: When invert mode is on, the display will tell you what items a
+          storage does not contain, and what storages an item can not be found
+          in.
+        </Text>
+        <Text fontSize="xl">
+          Q: What are the numbers in the different widgets?
+        </Text>
+        <Text fontSize="xl">
+          A: In the manage items page, the number shown is the total stock of
+          the item. In the manage storages page, the number shown is the number
+          of items contained in that storage. When viewing a specific item, the
+          number shown next to each storage is the amount of that item the
+          storage contains. When viewing a specific storage, the number shown
+          next to each item is the amount of that item the storage contains.
+        </Text>
         <Text fontSize="4xl">Dymo Instructions</Text>
         <Text fontSize="xl">In order to have dymo printing functionality:</Text>
         <UnorderedList fontSize="xl">
@@ -71,7 +100,9 @@ export default function Home({ admins }: PageProps) {
           questions, need further instruction, or have suggestions for
           improvement.
         </Text>
+        <Box h="10px"></Box>
       </Box>
+      <Box minH="calc(50px + env(safe-area-inset-bottom))"></Box>
     </Layout>
   );
 }
