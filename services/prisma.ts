c597be2cd58381a6c3904f42136f9d57f0fdbf7c
3LucasZ <lucas.zheng@warriorlife.net>
2024-04-16 02:52:34 -0700

    New custom PrismaClient (transformed fields)

--- services/prisma.ts
+++ services/prisma.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma = new PrismaClient();
+let prisma = new PrismaClient().$extends({
+  result: {
+    item: {
+      image: {
+        needs: {
+          image: true,
+        },
+        compute(item) {
+          return item.image.toString("base64");
+        },
+      },
+    },
+  },
+});
 
 export default prisma;
