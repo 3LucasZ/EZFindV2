fd31d8695976623cc546c9ea05478a05a86c8665
3LucasZ <lucas.zheng@warriorlife.net>
2024-04-16 18:20:35 -0700

    admin => user rename

--- components/Widget/UserWidget.tsx
+++ components/Widget/UserWidget.tsx
@@ -15,15 +15,16 @@ import { poster } from "services/poster";
 export type UserProps = {
   id: number;
   email: string;
+  isAdmin: boolean;
 };
 
 const UserWidget: React.FC<{
   user: UserProps;
-}> = ({ user: admin }) => {
+}> = ({ user }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toaster = useToast();
   const handleDelete = async () => {
-    const body = { id: admin.id };
+    const body = { id: user.id };
     const res = await poster("/api/delete-admin", body, toaster);
     if (res.status == 200) {
       Router.reload();
@@ -40,7 +41,7 @@ const UserWidget: React.FC<{
         roundedRight="none"
         width="100%"
       >
-        <Text width="100%">{admin.email}</Text>
+        <Text width="100%">{user.email}</Text>
       </Box>
       <IconButton
         onClick={onOpen}
@@ -54,7 +55,7 @@ const UserWidget: React.FC<{
       <ConfirmDeleteModal
         isOpen={isOpen}
         onClose={onClose}
-        name={" the admin: " + admin.email}
+        name={" the admin: " + user.email}
         handleDelete={handleDelete}
       />
     </Flex>
