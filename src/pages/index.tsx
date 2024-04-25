e2606f85e3666fd1741804c1ca2ec6e799e0217b
3LucasZ <lucas.zheng@warriorlife.net>
2024-04-25 00:30:25 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Heading,
+  SimpleGrid,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { RouteButton } from "components/RouteButton";
 import Layout from "components/Layout";
 import { GetServerSideProps } from "next";
@@ -9,62 +16,49 @@ import { FaTools } from "react-icons/fa";
 import { FaBoxes } from "react-icons/fa";
 import { UserProps } from "components/Widget/UserWidget";
 import Header from "components/Header";
+import GroupWidget, { GroupProps } from "components/Widget/GroupWidget";
+import SearchView from "components/SearchView";
+import { poster } from "services/poster";
 
-type PageProps = {};
+type PageProps = {
+  groups: GroupProps[];
+};
 
-export default function Home({}: PageProps) {
+export default function Home({ groups }: PageProps) {
   const { data: session } = useSession();
+  const isAdmin = session?.user.isAdmin;
+  const toaster = useToast();
   return (
     <Layout isAdmin={session?.user.isAdmin}>
-      <SimpleGrid
-        columns={[1, 2]}
-        overflowY="auto"
-        w="100svw"
-        h={"100%"}
-        position={"fixed"}
-        py={16}
-        top={"0%"}
-        zIndex={-100}
-      >
-        {/* {session && admins.includes(session!.user!.email!) && (
-          <RouteButton
-            route={""}
-            text={"storage Status"}
-            icon={MdManageAccounts}
-            color={"red.400"}
-            hoverColor={"red.100"}
-          ></RouteButton>
-        )} */}
-        <RouteButton
-          route={"manage-items"}
-          text={"Items"}
-          icon={FaTools}
-          color={"cyan.300"}
-          hoverColor={"cyan.100"}
-        ></RouteButton>
-        {/* {session && admins.includes(session!.user!.email!) && (
-          <RouteButton
-            route={""}
-            text={"View Logs"}
-            icon={IoDocumentText}
-            color={"orange.400"}
-            hoverColor={"orange.100"}
-          ></RouteButton>
-        )} */}
-        <RouteButton
-          route={"manage-storages"}
-          text={"Storages"}
-          icon={FaBoxes}
-          color={"blue.300"}
-          hoverColor={"blue.100"}
-        ></RouteButton>
-      </SimpleGrid>
+      <Center>
+        <Heading py="15">Welcome to EZFind!</Heading>
+      </Center>
+      <Center>
+        <Text>Get started by selecting a group to find/add items in. </Text>
+      </Center>
+      <Box minH="8px"></Box>
+      <SearchView
+        setIn={groups.map((group) => ({
+          name: group.name,
+          widget: <GroupWidget group={group} key={group.id} />,
+        }))}
+        onAdd={async () => {
+          const body = JSON.stringify("");
+          const res = await poster("/api/create-group", body, toaster);
+          // if (res.status == 200)
+          //   await Router.push({ pathname: "/group/" + (await res.json()) });
+        }}
+        isAdmin={isAdmin}
+        isEdit={false}
+        columns={4}
+      />
     </Layout>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  var groups = await prisma.group.findMany();
   return {
-    props: {},
+    props: { groups },
   };
 };
