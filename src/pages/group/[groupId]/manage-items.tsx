a2cc24cddf9eec084cc9f6d8c3ee93f04a7b2913
3LucasZ <lucas.zheng@warriorlife.net>
2024-04-26 21:18:01 -0700

    group-attached manage-items/storage

--- src/pages/group/[groupId]/manage-items.tsx
+++ src/pages/group/[groupId]/manage-items.tsx
@@ -7,28 +7,37 @@ import { getSession, useSession } from "next-auth/react";
 import { UserProps } from "components/Widget/UserWidget";
 import Router from "next/router";
 import { errorToast } from "services/toasty";
-import { Box, useToast } from "@chakra-ui/react";
+import { Box, useToast, Text, Heading, Center } from "@chakra-ui/react";
 import Header from "components/Header";
 import { poster } from "services/poster";
 import { Session } from "next-auth";
+import { GroupProps } from "components/Widget/GroupWidget";
 
 type PageProps = {
-  items: ItemProps[];
+  group: GroupProps;
 };
-export default function ManageItems({ items }: PageProps) {
+export default function ManageItems({ group }: PageProps) {
   const { data: session } = useSession();
   const isAdmin = session?.user.isAdmin;
   const toaster = useToast();
   return (
     <Layout isAdmin={session?.user.isAdmin}>
       <Box minH="8px"></Box>
+      <Center>
+        <Text fontSize={["2xl", "2xl", "2xl", "3xl", "4xl"]}>
+          {group.name}: Items
+        </Text>
+      </Center>
+      <Box minH="8px"></Box>
       <SearchView
-        setIn={items.map((item) => ({
+        setIn={group.items.map((item) => ({
           name: item.name,
           widget: <ItemWidget item={item} key={item.id} />,
         }))}
         onAdd={async () => {
-          const body = JSON.stringify("");
+          const body = {
+            groupId: group.id,
+          };
           const res = await poster("/api/create-item", body, toaster);
           if (res.status == 200)
             await Router.push({ pathname: "/item/" + (await res.json()) });
@@ -40,13 +49,27 @@ export default function ManageItems({ items }: PageProps) {
   );
 }
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  var items = await prisma.item.findMany({
-    include: { storageRelations: true },
+  var groupId = Number(ctx.params?.groupId);
+  var group = await prisma.group.findUnique({
+    where: {
+      id: groupId,
+    },
+    include: {
+      items: {
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          storageRelations: {
+            select: {
+              count: true,
+            },
+          },
+        },
+      },
+    },
   });
-  items.forEach((item) => (item.image = "")); //(experimental) remove image from items to reduce payload???
-  //const session = await getSession(ctx); //getSession in ServerSideProps takes over 10 seconds
-
   return {
-    props: { items },
+    props: { group },
   };
 };
--- src/pages/group/[groupId]/manage-storages.tsx
+++ src/pages/group/[groupId]/manage-storages.tsx
@@ -7,26 +7,35 @@ import { UserProps } from "components/Widget/UserWidget";
 import { useSession } from "next-auth/react";
 import { errorToast } from "services/toasty";
 import Router from "next/router";
-import { Box, useToast } from "@chakra-ui/react";
+import { Box, Center, useToast, Text } from "@chakra-ui/react";
 import Header from "components/Header";
 import { poster } from "services/poster";
+import { GroupProps } from "components/Widget/GroupWidget";
 
 type PageProps = {
-  storages: StorageProps[];
+  group: GroupProps;
 };
-export default function ManageStorages({ storages }: PageProps) {
+export default function ManageStorages({ group }: PageProps) {
   const { data: session } = useSession();
   const toaster = useToast();
   return (
     <Layout isAdmin={session?.user.isAdmin}>
       <Box minH="8px"></Box>
+      <Center>
+        <Text fontSize={["2xl", "2xl", "2xl", "3xl", "4xl"]}>
+          {group.name}: Items
+        </Text>
+      </Center>
+      <Box minH="8px"></Box>
       <SearchView
-        setIn={storages.map((storage) => ({
+        setIn={group.storages.map((storage) => ({
           name: storage.name,
           widget: <StorageWidget storage={storage} key={storage.id} />,
         }))}
         onAdd={async () => {
-          const body = JSON.stringify("");
+          const body = {
+            groupId: group.id,
+          };
           const res = await poster("/api/create-storage", body, toaster);
           if (res.status == 200)
             await Router.push({ pathname: "/storage/" + (await res.json()) });
@@ -37,11 +46,28 @@ export default function ManageStorages({ storages }: PageProps) {
     </Layout>
   );
 }
-export const getServerSideProps: GetServerSideProps = async () => {
-  const storages = await prisma.storage.findMany({
-    include: { itemRelations: true },
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  var groupId = Number(ctx.params?.groupId);
+  var group = await prisma.group.findUnique({
+    where: {
+      id: groupId,
+    },
+    include: {
+      storages: {
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          itemRelations: {
+            select: {
+              count: true,
+            },
+          },
+        },
+      },
+    },
   });
   return {
-    props: { storages },
+    props: { group },
   };
 };
