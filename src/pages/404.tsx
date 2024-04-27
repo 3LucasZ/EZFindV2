335d7691bf8932c19969212a232a7e41d2ed4ec0
3LucasZ <lucas.zheng@warriorlife.net>
2024-04-26 22:51:08 -0700

    custom 404 page :)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,42 @@
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import Layout from "components/Layout";
 
 export default function Custom404() {
-  const router = useRouter();
+  return (
+    <Layout isAdmin={false}>
+      <Box h="15%"></Box>
+      <Box textAlign="center" py={10} px={6}>
+        <Heading
+          display="inline-block"
+          as="h1"
+          size="4xl"
+          // bgGradient="linear(to-r, teal.400, teal.600)"
+          bg="teal.400"
+          backgroundClip="text"
+        >
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Page Not Found
+        </Text>
+        <Text color={"gray.500"} mb={6}>
+          The page you&apos;re looking for does not seem to exist
+        </Text>
 
-  useEffect(() => {
-    router.replace("/");
-  });
-
-  return null;
+        <Button
+          colorScheme="teal"
+          // bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+          color="white"
+          variant="solid"
+          onClick={() => {
+            Router.push("/");
+          }}
+        >
+          Go to Home
+        </Button>
+      </Box>
+    </Layout>
+  );
 }
