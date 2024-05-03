53c38497777034bd39e2251e2a5fdff2757167dd
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-03 15:45:14 -0700

    imageUrl iinstead of rawImage storing prisma (items, storages)

---
+++ prisma/migrations/20240503223426_image_url/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Item" ALTER COLUMN "image" SET DEFAULT '',
+ALTER COLUMN "image" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Storage" ALTER COLUMN "image" SET DEFAULT '',
+ALTER COLUMN "image" SET DATA TYPE TEXT;
