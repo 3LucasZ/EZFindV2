eed9c0f96c3ff4f9e21954042356234bfb798f02
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-03 01:42:29 -0700

    instead of Buffer use url

--- services/prisma.ts
+++ services/prisma.ts
@@ -2,16 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma = new PrismaClient().$extends({
   result: {
-    group: {
-      image: {
-        needs: {
-          image: true,
-        },
-        compute(group) {
-          return group.image.toString("base64");
-        },
-      },
-    },
+    // group: {
+    //   image: {
+    //     needs: {
+    //       image: true,
+    //     },
+    //     compute(group) {
+    //       return group.image.toString("base64");
+    //     },
+    //   },
+    // },
     item: {
       image: {
         needs: {
