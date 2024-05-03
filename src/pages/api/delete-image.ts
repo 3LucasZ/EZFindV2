8db5a601a641e966f5e063d5fc02e38be9434251
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-03 15:45:48 -0700

    delete-image still returns 200 status even if it fails
    if can't delete the image, do not interrupt the upload process. Give them the 200.

--- src/pages/api/delete-image.ts
+++ src/pages/api/delete-image.ts
@@ -25,6 +25,7 @@ export default async function handle(
     return res.status(200).json(imagePath);
   } catch (e) {
     console.error("Error while trying to delete a file:" + e);
-    return res.status(500).json("Error while trying to delete a file: " + e);
+    // return res.status(500).json("Error while trying to delete a file: " + e);
+    return res.status(200).json("Error while trying to delete a file: " + e); //if can't delete the image, do not interrupt the upload process. Give them the 200.
   }
 }
