2ca9a7c195cd22ff549c8eb73f604e80a2220d59
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-04 04:09:29 -0700

    Update explore.tsx

--- src/pages/group/[groupId]/explore.tsx
+++ src/pages/group/[groupId]/explore.tsx
@@ -1,26 +1,14 @@
 import {
-  Center,
+  Box,
+  Container,
   Flex,
-  IconButton,
-  useDisclosure,
-  useToast,
-  Text,
   SimpleGrid,
+  VStack,
+  useToast,
 } from "@chakra-ui/react";
-import {
-  CheckIcon,
-  CloseIcon,
-  DeleteIcon,
-  EditIcon,
-  Icon,
-  SettingsIcon,
-} from "@chakra-ui/icons";
-import { IoImageOutline } from "react-icons/io5";
 import { GetServerSideProps } from "next";
-import ConfirmActionModal from "components/Main/ConfirmActionModal";
 import Router from "next/router";
 import Layout from "components/Layout/MainLayout";
-import SearchView from "components/Main/SearchView";
 import { useSession } from "next-auth/react";
 import prisma from "services/prisma";
 import { UserProps } from "components/Widget/UserWidget";
@@ -28,15 +16,11 @@ import { useState } from "react";
 import React from "react";
 import AutoResizeTextarea from "components/Minis/AutoResizeTextarea";
 import { poster } from "services/poster";
-import ImageModal from "components/Main/ImageModal";
 import { GroupProps } from "components/Widget/GroupWidget";
-import UserGroupRelationWidget, {
-  UserGroupRelationProps,
-} from "components/Widget/UserGroupRelationWidget";
 import EditableTitle from "components/Minis/EditableTitle";
-import { RouteButton } from "components/Main/RouteButton";
-import { FaBoxes, FaTools } from "react-icons/fa";
-import { IoIosSettings } from "react-icons/io";
+import Carousel from "components/Main/Carousel";
+import EditableSubtitle from "components/Minis/EditableSubtitle";
+import { CustomStat } from "components/Main/CustomStat";
 
 type PageProps = {
   group: GroupProps;
@@ -56,50 +40,11 @@ export default function GroupPage({ group, users }: PageProps) {
           group.minPerm,
           userGroupRelation?.perm != undefined ? userGroupRelation.perm : -1
         );
-  const toaster = useToast();
-  //--state--
-  const [isEdit, setIsEdit] = useState(false);
-  //change metadata
-  const [newName, setNewName] = useState(group.name);
-  const [newDescription, setNewDescription] = useState(group.description);
-  const [newUserRelations, setNewRelations] = useState(group.userRelations);
-  // handle delete modal
-  const {
-    isOpen: isOpenTrash,
-    onOpen: onOpenTrash,
-    onClose: onCloseTrash,
-  } = useDisclosure();
-  const handleDelete = async () => {
-    const body = { id: group.id };
-    const res = await poster("/api/delete-group", body, toaster);
-    if (res.status == 200) await Router.push({ pathname: "/manage-groups" });
-  };
-  // handle view modal
-  const {
-    isOpen: isOpenViewer,
-    onOpen: onOpenViewer,
-    onClose: onCloseViewer,
-  } = useDisclosure();
-  //handle upload image
-  const uploadImage = async (imageStr: string) => {
-    const body = { id: group.id, newImageStr: imageStr };
-    // console.log(body);
-    const res = await poster("/api/update-group-image", body, toaster);
-    if (res.status == 200) Router.reload();
-  };
-  // handle update
-  const handleUpdateGroup = async () => {
-    const body = {
-      id: group.id,
-      newName,
-      newDescription,
-      newUserRelations,
-    };
-    const res = await poster("/api/update-group-full", body, toaster);
-    if (res.status == 200) Router.reload();
-  };
-  //prefix
-  const prefix = "/group/" + group.id + "/";
+  const stats = [
+    { label: "Total Subscribers", value: "71,887" },
+    { label: "Avg. Open Rate", value: "56.87%" },
+    { label: "Avg. Click Rate", value: "12.87%" },
+  ];
   console.log(group.userRelations, userGroupRelation?.perm, perm);
   return (
     <Layout
@@ -108,73 +53,30 @@ export default function GroupPage({ group, users }: PageProps) {
       authorized={perm > -1}
       group={group}
     >
-      <Flex px={[2, "5vw", "10vw", "15vw"]}>
-        <EditableTitle value={group.name} isDisabled={true} />
-
-        {/* <Center>
-          <IconButton
-            ml="2"
-            colorScheme="blue"
-            aria-label=""
-            icon={<Icon as={IoImageOutline} boxSize={5} />}
-            onClick={() => {
-              onOpenViewer();
-            }}
+      <VStack>
+        <Flex px={[2, "5vw", "10vw", "15vw"]}>
+          <EditableTitle value={group.name} isDisabled={true} />
+        </Flex>
+        <Flex px={[2, "5vw", "10vw", "15vw"]}>
+          <EditableSubtitle
+            value={group.description ? group.description : "No description."}
+            disabled
           />
-          <ImageModal
-            onClose={onCloseViewer}
-            isOpen={isOpenViewer}
-            onUpload={uploadImage}
-            imageStr={group.image}
-          />
-        </Center> */}
-      </Flex>
-      <Flex px={[2, "5vw", "10vw", "15vw"]}>
-        <AutoResizeTextarea
-          value={
-            isEdit
-              ? newDescription
-              : group.description
-              ? group.description
-              : "No description."
-          }
-          onChange={(e) => setNewDescription(e.target.value)}
-          isDisabled={!isEdit}
-          maxLength={250}
-          _disabled={{ color: "black", borderColor: "white" }}
-          fontSize={["xs", "xs", "sm", "md", "lg", "xl"]}
-          sx={{ opacity: "1" }}
-        />
-      </Flex>
-      <SimpleGrid
-        columns={[1, 2, 3]}
-        spacing={10}
-        pt="15"
-        overflowY="auto"
-        flexDir={"column"}
-      >
-        <RouteButton
-          route={prefix + "manage-items"}
-          text={"Items"}
-          icon={FaTools}
-          color={"cyan.300"}
-          hoverColor={"cyan.100"}
-        ></RouteButton>
-        <RouteButton
-          route={prefix + "manage-storages"}
-          text={"Storages"}
-          icon={FaBoxes}
-          color={"blue.300"}
-          hoverColor={"blue.100"}
-        ></RouteButton>
-        <RouteButton
-          route={prefix + "settings"}
-          text={"Settings"}
-          icon={IoIosSettings}
-          color={"orange.400"}
-          hoverColor={"orange.100"}
-        ></RouteButton>
-      </SimpleGrid>
+        </Flex>
+        <Box as="section" py={{ base: "4", md: "8" }}>
+          <Container>
+            <SimpleGrid
+              columns={{ base: 1, md: 3 }}
+              gap={{ base: "5", md: "6" }}
+            >
+              {stats.map(({ label, value }) => (
+                <CustomStat key={label} label={label} value={value} />
+              ))}
+            </SimpleGrid>
+          </Container>
+        </Box>
+        <Carousel />
+      </VStack>
     </Layout>
   );
 }
@@ -206,3 +108,75 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   };
 };
+
+{
+  /* <SimpleGrid
+  columns={[1, 2, 3]}
+  spacing={10}
+  pt="15"
+  overflowY="auto"
+  flexDir={"column"}
+>
+  <RouteButton
+    route={prefix + "manage-items"}
+    text={"Items"}
+    icon={FaTools}
+    color={"cyan.300"}
+    hoverColor={"cyan.100"}
+  ></RouteButton>
+  <RouteButton
+    route={prefix + "manage-storages"}
+    text={"Storages"}
+    icon={FaBoxes}
+    color={"blue.300"}
+    hoverColor={"blue.100"}
+  ></RouteButton>
+  <RouteButton
+    route={prefix + "settings"}
+    text={"Settings"}
+    icon={IoIosSettings}
+    color={"orange.400"}
+    hoverColor={"orange.100"}
+  ></RouteButton>
+</SimpleGrid> */
+}
+{
+  /* 
+   // handle view modal
+  const {
+    isOpen: isOpenViewer,
+    onOpen: onOpenViewer,
+    onClose: onCloseViewer,
+  } = useDisclosure();
+  
+<Center>
+  <IconButton
+    ml="2"
+    colorScheme="blue"
+    aria-label=""
+    icon={<Icon as={IoImageOutline} boxSize={5} />}
+    onClick={() => {
+      onOpenViewer();
+    }}
+  />
+  <ImageModal
+    onClose={onCloseViewer}
+    isOpen={isOpenViewer}
+    onUpload={uploadImage}
+    imageStr={group.image}
+  />
+</Center> 
+
+// handle delete modal
+  const {
+    isOpen: isOpenTrash,
+    onOpen: onOpenTrash,
+    onClose: onCloseTrash,
+  } = useDisclosure();
+  const handleDelete = async () => {
+    const body = { id: group.id };
+    const res = await poster("/api/delete-group", body, toaster);
+    if (res.status == 200) await Router.push({ pathname: "/manage-groups" });
+  };
+*/
+}
