b2f4f6f0ea13a4df2dbbfb389b377b693c080f22
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-04 02:00:59 -0700

    merge AppBarBtn to AppBar

--- components/Layout/AppBar.tsx
+++ components/Layout/AppBar.tsx
@@ -1,9 +1,10 @@
-import { HStack, Icon, IconButton } from "@chakra-ui/react";
+import { Box, Center, HStack, Icon, IconButton } from "@chakra-ui/react";
 import { FaHome } from "react-icons/fa";
 import { FaTools } from "react-icons/fa";
 import { FaBoxes } from "react-icons/fa";
 import { FaCircleInfo } from "react-icons/fa6";
-import AppBarBtn from "./AppBarBtn";
+import Router from "next/router";
+import { IconType } from "react-icons";
 
 export default function AppBar() {
   return (
@@ -20,5 +21,35 @@ export default function AppBar() {
       <AppBarBtn icon={FaBoxes} href="/manage-storages" />
       <AppBarBtn icon={FaCircleInfo} href="/help" />
     </HStack>
+  );
+}
+
+type AppBarBtnProps = {
+  icon: IconType;
+  href: string;
+};
+function AppBarBtn({ icon, href }: AppBarBtnProps) {
+  return (
+    <Box
+      w={"33%"}
+      h={"100%"}
+      rounded="unset"
+      aria-label={""}
+      bg="teal.300"
+      _hover={{ bg: "teal.400" }}
+      color="white"
+      pt="13px"
+      onClick={() => Router.push(href)}
+      style={{ textDecoration: "none" }}
+      sx={{
+        WebkitUserDrag: "none",
+      }}
+    >
+      {
+        <Center>
+          <Icon as={icon} boxSize="23" />
+        </Center>
+      }
+    </Box>
   );
 }
