758b81336edb1a992dacbdbf4affd22c1ef458fb
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-11 00:31:08 -0700

    [New] AddRemoveButton styling

--- components/Minis/AddRemoveButton.tsx
+++ components/Minis/AddRemoveButton.tsx
@@ -1,6 +1,7 @@
 import { SmallAddIcon, SmallCloseIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
+import { AspectRatio, Icon, IconButton } from "@chakra-ui/react";
 import { MouseEventHandler } from "react";
+import { FiHome, FiMinus, FiPlus } from "react-icons/fi";
 
 type AddRemoveButtonProps = {
   mode: number;
@@ -13,7 +14,43 @@ export default function AddRemoveButton({
   handleRemove,
 }: AddRemoveButtonProps) {
   return (
-    <IconButton
+    <Icon
+      //---color
+      // bg={mode == 1 ? "green.300" : "red.300"}
+      // _hover={{ bg: mode == 1 ? "green.400" : "red.400" }}
+      bg="white"
+      transition=""
+      _hover={{ bg: mode == 1 ? "teal.300" : "red.400", color: "white" }}
+      // _hover={{ bg: "blue.200" }}
+      color="black"
+      //---border
+      // rounded="md"
+      // roundedLeft="none"
+      rounded="xl"
+      borderColor={"grey.200"}
+      borderWidth={"1px"}
+      //---display
+      as={mode == 1 ? FiPlus : FiMinus}
+      // boxSize={"10"}
+      display={mode == 0 ? "none" : ""}
+      // minW="12"
+      // maxW="12"
+      // minH="12"
+      // maxH="12"
+      boxSize={"10"}
+      p="2.5"
+      //---misc
+      onClick={(e) => {
+        e.stopPropagation(); //if parent element is clicked, it will be ignored, so that this action activates instead.
+        return mode == 1 ? handleAdd : handleRemove;
+      }}
+      aria-label={""}
+    />
+  );
+}
+
+{
+  /* <IconButton
       //color
       bg={mode == 1 ? "green.300" : "red.300"}
       _hover={{ bg: mode == 1 ? "green.400" : "red.400" }}
@@ -21,13 +58,11 @@ export default function AddRemoveButton({
       aria-label={mode == 1 ? "add" : "delete"}
       //display
       icon={mode == 1 ? <SmallAddIcon /> : <SmallCloseIcon />}
-      h={8}
       w={"40px"}
       display={mode == 0 ? "none" : ""}
       //misc
       rounded="md"
       roundedLeft="none"
       onClick={mode == 1 ? handleAdd : handleRemove}
-    />
-  );
+    /> */
 }
