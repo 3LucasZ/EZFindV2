effcbd01cfcd59a6d70541cce35bb0399a8f3fac
3LucasZ <lucas.zheng@warriorlife.net>
2024-06-10 19:43:39 -0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,24 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* BASIC */}
-        <meta name="application-name" content="EZFind" />
-        <meta name="description" content="VCS Inventory Tool" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="EZFind" />
-
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-
-        {/* <meta name="theme-color" content="#000000" /> */}
-        {/* ICON */}
-        {/* <link rel="apple-touch-icon" href="/icons/icon.png" />
-        <link rel="icon" type="image/x-icon" href="/icons/icon.png" /> */}
-        {/* MANIFEST */}
-        {/* <link rel="manifest" href="/manifest.json" /> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
