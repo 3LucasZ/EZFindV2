75dd6025c0e9c1e7af64fd5a8ddcc09442f66f93
3LucasZ <lucas.zheng@warriorlife.net>
2024-06-15 00:30:48 -0700

    archive manage-groups (most likely permanently)

---
+++ archive/manage-groups.tsx
@@ -0,0 +1,67 @@
+// import { ItemProps } from "types/db";
+// import Layout from "components/Layout/MainLayout";
+// import SearchView from "components/Main/SearchView";
+// import { GetServerSideProps } from "next";
+// import prisma from "services/prisma";
+// import { getSession, useSession } from "next-auth/react";
+// import { UserProps } from "types/db";
+// import Router from "next/router";
+// import { errorToast } from "services/toasty";
+// import { Box, Center, Heading, useToast, Text } from "@chakra-ui/react";
+// import Header from "components/Layout/Header";
+// import { poster } from "services/poster";
+// import { Session } from "next-auth";
+// import GroupWidget from "components/Widget/GroupWidget";
+// import { GroupProps } from "@/db";
+// import { FAB } from "components/Layout/FAB/FAB";
+// import { AddIcon } from "@chakra-ui/icons";
+// import { responsiveHeaderFontSize } from "services/constants";
+// import { useEffect } from "react";
+
+// type PageProps = {
+//   groups: GroupProps[];
+// };
+// export default function ManageGroups({ groups }: PageProps) {
+//   //--copy paste on every page--
+//   const { data: session, status, update } = useSession();
+//   useEffect(() => {
+//     update();
+//   }, []);
+//   const me = session?.user;
+//   const toaster = useToast();
+//   //--ret--
+//   return (
+//     <Layout me={me} loaded={status !== "loading"} authorized={true}>
+//       <Center>
+//         <Text fontSize={responsiveHeaderFontSize}>Groups</Text>
+//       </Center>
+//       <Box minH="8px"></Box>
+//       <SearchView
+//         set={groups.map((group) => ({
+//           name: group.name,
+//           widget: <GroupWidget group={group} key={group.id} />,
+//         }))}
+//         columns={[2, 3, 3, 4]}
+//       />
+//       <FAB
+//         // name="Create"
+//         icon={AddIcon}
+//         onClick={async () => {
+//           const body = JSON.stringify("");
+//           const res = await poster("/api/create-group", body, toaster);
+//           if (res.status == 200)
+//             await Router.push({
+//               pathname: "/group/" + (await res.json()) + "/explore",
+//             });
+//         }}
+//         hidden={!me?.isAdmin}
+//       />
+//     </Layout>
+//   );
+// }
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   var groups = await prisma.group.findMany();
+//   return {
+//     props: { groups },
+//   };
+// };
